a9d709cc7ef35dfc40f3274e45b5907f2a4238ef
ClemCiupek <marie-clemence.ciupek@vinted.com>
2024-11-08 14:47:31 +0100

    quick fixes

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .PHONY: install
-install_script := $(target_dir)/src/install.sh
+install_script := src/install.sh
 
 install:
 	@echo "Installing..."
-	chmod +x $(install_script)
-	./$(install_script)
+	@chmod +x $(install_script)
+	@./$(install_script)
--- src/install.sh
+++ src/install.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 # Ask the user for the path to their dataverse repo (default: ../)
-read -p "Enter the path to your dataverse repo (default: ../): " dataverse_repo
-dataverse_repo=${dataverse_repo:-../}
+promt_def_dir=$(cd .. && dirs)/dataverse-marketplace-dbt
+def_dir=$(cd .. && pwd)/dataverse-marketplace-dbt
+read -p "Enter the path to your dataverse repo [${promt_def_dir}]: " dataverse_repo
+dataverse_repo=${dataverse_repo:-${def_dir}}
 
 # Ask the user for their domain (default: marketing)
-read -p "Enter your domain (default: marketing): " domain
+read -p "Enter your domain [marketing]: " domain
 domain=${domain:-marketing}
 
-# Define the target directory
-target_dir="${dataverse_repo}/${domain}/script"
+# Define the target directorys
+target_dir="${dataverse_repo}/${domain}/scripts"
+echo "target_dir: ${target_dir}"
 
 # Create the target directory if it doesn't exist
 mkdir -p "$target_dir"
 
 # Copy the create_samples script to the target directory
-cp create_samples.sh "$target_dir"
+echo "Copying the create_samples script to ${target_dir}..."
+cp src/create_samples.sh "$target_dir"
 
 # Give execute permission to the create_samples script
 chmod +x "${target_dir}/create_samples.sh"
